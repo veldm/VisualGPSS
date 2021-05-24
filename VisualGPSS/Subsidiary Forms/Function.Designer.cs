9590e5b4f5d175c08c1f393fcb5f0ae42aa1f6c8
veldm <Dmavel8@yandex.ru>
2021-05-24 23:47:06 +0300

    24.V.2021 final

---
+++ VisualGPSS/Subsidiary Forms/Function.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace VisualGPSS.Subsidiary_Forms
+{
+    partial class Function
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FrqVal_DGV = new System.Windows.Forms.DataGridView();
+            this.Frequency = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
+            this.pointsCountNUD = new System.Windows.Forms.NumericUpDown();
+            this.typeCB = new System.Windows.Forms.ComboBox();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.operandTB = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.nameTB = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.DeleteButton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.Cancelbutton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.SaveButton = new MaterialSkin.Controls.MaterialFlatButton();
+            ((System.ComponentModel.ISupportInitialize)(this.FrqVal_DGV)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pointsCountNUD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FrqVal_DGV
+            // 
+            this.FrqVal_DGV.AllowUserToAddRows = false;
+            this.FrqVal_DGV.AllowUserToDeleteRows = false;
+            this.FrqVal_DGV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FrqVal_DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.FrqVal_DGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Frequency,
+            this.Value});
+            this.FrqVal_DGV.Location = new System.Drawing.Point(279, 80);
+            this.FrqVal_DGV.Name = "FrqVal_DGV";
+            this.FrqVal_DGV.RowHeadersVisible = false;
+            this.FrqVal_DGV.Size = new System.Drawing.Size(253, 194);
+            this.FrqVal_DGV.TabIndex = 0;
+            // 
+            // Frequency
+            // 
+            this.Frequency.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Frequency.HeaderText = "Частота";
+            this.Frequency.Name = "Frequency";
+            this.Frequency.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Value
+            // 
+            this.Value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Value.HeaderText = "Значение";
+            this.Value.Name = "Value";
+            this.Value.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Controls.Add(this.materialLabel4);
+            this.groupBox1.Controls.Add(this.pointsCountNUD);
+            this.groupBox1.Controls.Add(this.typeCB);
+            this.groupBox1.Controls.Add(this.materialLabel3);
+            this.groupBox1.Controls.Add(this.materialLabel2);
+            this.groupBox1.Controls.Add(this.operandTB);
+            this.groupBox1.Controls.Add(this.materialLabel1);
+            this.groupBox1.Controls.Add(this.nameTB);
+            this.groupBox1.Location = new System.Drawing.Point(12, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 199);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // materialLabel4
+            // 
+            this.materialLabel4.AutoSize = true;
+            this.materialLabel4.Depth = 0;
+            this.materialLabel4.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel4.Location = new System.Drawing.Point(6, 162);
+            this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel4.Name = "materialLabel4";
+            this.materialLabel4.Size = new System.Drawing.Size(139, 19);
+            this.materialLabel4.TabIndex = 8;
+            this.materialLabel4.Text = "Количество точек";
+            // 
+            // pointsCountNUD
+            // 
+            this.pointsCountNUD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pointsCountNUD.Location = new System.Drawing.Point(164, 159);
+            this.pointsCountNUD.Name = "pointsCountNUD";
+            this.pointsCountNUD.Size = new System.Drawing.Size(83, 26);
+            this.pointsCountNUD.TabIndex = 7;
+            this.pointsCountNUD.ValueChanged += new System.EventHandler(this.pointsCountNUD_ValueChanged);
+            // 
+            // typeCB
+            // 
+            this.typeCB.FormattingEnabled = true;
+            this.typeCB.Items.AddRange(new object[] {
+            "Дискретная",
+            "Непрерывная"});
+            this.typeCB.Location = new System.Drawing.Point(82, 112);
+            this.typeCB.Name = "typeCB";
+            this.typeCB.Size = new System.Drawing.Size(165, 21);
+            this.typeCB.TabIndex = 6;
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(6, 114);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(36, 19);
+            this.materialLabel3.TabIndex = 5;
+            this.materialLabel3.Text = "Вид";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(6, 64);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(70, 19);
+            this.materialLabel2.TabIndex = 3;
+            this.materialLabel2.Text = "Операнд";
+            // 
+            // operandTB
+            // 
+            this.operandTB.Depth = 0;
+            this.operandTB.Hint = "";
+            this.operandTB.Location = new System.Drawing.Point(82, 64);
+            this.operandTB.MouseState = MaterialSkin.MouseState.HOVER;
+            this.operandTB.Name = "operandTB";
+            this.operandTB.PasswordChar = '\0';
+            this.operandTB.SelectedText = "";
+            this.operandTB.SelectionLength = 0;
+            this.operandTB.SelectionStart = 0;
+            this.operandTB.Size = new System.Drawing.Size(165, 23);
+            this.operandTB.TabIndex = 2;
+            this.operandTB.UseSystemPasswordChar = false;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(6, 16);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(53, 19);
+            this.materialLabel1.TabIndex = 1;
+            this.materialLabel1.Text = "Метка";
+            // 
+            // nameTB
+            // 
+            this.nameTB.Depth = 0;
+            this.nameTB.Hint = "";
+            this.nameTB.Location = new System.Drawing.Point(82, 16);
+            this.nameTB.MouseState = MaterialSkin.MouseState.HOVER;
+            this.nameTB.Name = "nameTB";
+            this.nameTB.PasswordChar = '\0';
+            this.nameTB.SelectedText = "";
+            this.nameTB.SelectionLength = 0;
+            this.nameTB.SelectionStart = 0;
+            this.nameTB.Size = new System.Drawing.Size(165, 23);
+            this.nameTB.TabIndex = 0;
+            this.nameTB.UseSystemPasswordChar = false;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.AutoSize = true;
+            this.DeleteButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.DeleteButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DeleteButton.Depth = 0;
+            this.DeleteButton.ForeColor = System.Drawing.Color.Red;
+            this.DeleteButton.Location = new System.Drawing.Point(12, 291);
+            this.DeleteButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.DeleteButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Primary = false;
+            this.DeleteButton.Size = new System.Drawing.Size(76, 36);
+            this.DeleteButton.TabIndex = 16;
+            this.DeleteButton.Text = "Удалить";
+            this.DeleteButton.UseVisualStyleBackColor = false;
+            // 
+            // Cancelbutton
+            // 
+            this.Cancelbutton.AutoSize = true;
+            this.Cancelbutton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Cancelbutton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Cancelbutton.Depth = 0;
+            this.Cancelbutton.Location = new System.Drawing.Point(220, 291);
+            this.Cancelbutton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Cancelbutton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Cancelbutton.Name = "Cancelbutton";
+            this.Cancelbutton.Primary = false;
+            this.Cancelbutton.Size = new System.Drawing.Size(86, 36);
+            this.Cancelbutton.TabIndex = 15;
+            this.Cancelbutton.Text = "Отменить";
+            this.Cancelbutton.UseVisualStyleBackColor = false;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.AutoSize = true;
+            this.SaveButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.SaveButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SaveButton.Depth = 0;
+            this.SaveButton.Location = new System.Drawing.Point(438, 291);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.SaveButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Primary = false;
+            this.SaveButton.Size = new System.Drawing.Size(93, 36);
+            this.SaveButton.TabIndex = 14;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = false;
+            // 
+            // Function
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(544, 342);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.Cancelbutton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.FrqVal_DGV);
+            this.Name = "Function";
+            this.Text = "Редактор пользовательских функций";
+            ((System.ComponentModel.ISupportInitialize)(this.FrqVal_DGV)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pointsCountNUD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView FrqVal_DGV;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialSingleLineTextField operandTB;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField nameTB;
+        private System.Windows.Forms.ComboBox typeCB;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
+        private System.Windows.Forms.NumericUpDown pointsCountNUD;
+        private MaterialSkin.Controls.MaterialFlatButton DeleteButton;
+        private MaterialSkin.Controls.MaterialFlatButton Cancelbutton;
+        public MaterialSkin.Controls.MaterialFlatButton SaveButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Frequency;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
---
+++ VisualGPSS/Subsidiary Forms/Function.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualGPSS.Subsidiary_Forms
+{
+    public partial class Function : MaterialSkin.Controls.MaterialForm
+    {
+        public Function()
+        {
+            InitializeComponent();
+        }
+
+        private void pointsCountNUD_ValueChanged(object sender, EventArgs e)
+        {
+            FrqVal_DGV.Enabled = !(pointsCountNUD.Value is 0);
+            if (pointsCountNUD.Value > FrqVal_DGV.Rows.Count)
+                FrqVal_DGV.Rows.Add(pointsCountNUD.Value - FrqVal_DGV.Rows.Count);
+            else if (pointsCountNUD.Value < FrqVal_DGV.Rows.Count)
+                for (int i = 0; i < FrqVal_DGV.Rows.Count -pointsCountNUD.Value; i++)
+                    FrqVal_DGV.Rows.RemoveAt(FrqVal_DGV.Rows.Count);
+        }
+
+
+    }
+}
